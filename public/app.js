a683124f6176b2c1c456f7de2b966fa62c96d5b8
Charlotte Lourme <charlotte@charchives.obj>
2026-04-13 09:08:30 +0200

    auto-deploy: 13/04 09:08

--- public/app.js
+++ public/app.js
@@ -755,6 +755,7 @@ function render() {
     `${filtered.length} objet${filtered.length!==1?'s':''}`;
 
   if (state.view==='grid')      renderGrid(filtered);
+  else if (state.view==='gallery')  renderGallery(filtered);
   else if (state.view==='calendar') renderCalendar(filtered);
   else if (state.view==='catalogue') renderCatalogue(filtered);
   else if (state.view==='trios')    renderTrios();
