66fa7abf183de671aa13a2a353297cc4ce55ae92
Charlotte Lourme <charlotte@charchives.obj>
2026-04-13 10:52:40 +0200

    auto-deploy: 13/04 10:52

--- public/app.js
+++ public/app.js
@@ -2682,8 +2682,11 @@ function openNew() {
   renderAllAttributes(); // includes renderUniversChips() + universSection consolidation
   // Are.na
   state.editExpositions = [];
+  state.editFragmentBg = '#1a1a1a';
   state._formType = 'item';
   _setFormType('item');
+  const ta = document.getElementById('fFragmentText');
+  if (ta) ta.value = '';
   renderExpoChipsPicker();
   switchModalTab('public');
   document.getElementById('editModal').style.display = 'flex';
