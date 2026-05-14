1aed9bd94476880af174dc828a3d8bb66ad3b00e
Charlotte Lourme <charlotte@charchives.obj>
2026-05-14 17:57:05 +0200

    auto-deploy: 14/05 17:57

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // CHARCHIVES — Minimal Service Worker for PWA installability
-const CACHE_NAME = 'charchives-v383';
+const CACHE_NAME = 'charchives-v384';
 const PRECACHE = [
   '/',
   '/style.css',
